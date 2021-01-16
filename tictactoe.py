9c3a9991372c062ae21f55faba0cd6f444fe7523
Oscar Perez <oscarfernando.perez@gmail.com>
2021-01-16 16:30:35 +0200

    Add first dummy version

--- tictactoe.py
+++ tictactoe.py
@@ -10,7 +10,16 @@ MAX_ROWS = len(board)
 MAX_COLUMNS = len(board[0])
 
 
-def placeMove(row, column, piece):
+def getBoard():
+    return str(board)
+
+
+def placeMove(strRow, strColumn, strPiece):
+
+    row = int(strRow)
+    column = int(strColumn)
+    piece = int(strPiece)
+
     if row >= MAX_ROWS or row < 0:
         raise Exception("Illegal row")
     if column >= MAX_COLUMNS or column < 0:
@@ -20,3 +29,5 @@ def placeMove(row, column, piece):
     if board[row][column] != NONE:
         raise Exception("There is a piece in there already")
     board[row][column] = piece
+    # Check there is a winner ?
+
