d6c251b85b959f0cbb17c1577fbbef04d8dacb3b
Oscar Perez <oscarfernando.perez@gmail.com>
2021-01-16 15:58:20 +0200

    Add first version

---
+++ tictactoe.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+NONE = 0
+X = 1
+O = 2
+
+MAX_ROWS = len(board)
+MAX_COLUMNS = len(board[0])
+
+
+def placeMove(row, column, piece):
+    if row >= MAX_ROWS or row < 0:
+        raise Exception("Illegal row")
+    if column >= MAX_COLUMNS or column < 0:
+        raise Exception("Illegal column")
+    if piece != X and piece != O:
+        raise Exception("Piece not valid")
+    if board[row][column] != NONE:
+        raise Exception("There is a piece in there already")
+    board[row][column] = piece
